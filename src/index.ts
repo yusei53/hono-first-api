20a866ccc376bcc368ccb51ad58ead23ea3dc75a
yusei53 <yusei530922@gmail.com>
2024-02-12 21:07:39 +0900

    postメソッド作成

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import { prettyJSON } from "hono/pretty-json";
 const app = new Hono();
 
 let testPosts = [
-  { id: 1, title: "Post 1", content: "This is post 1" },
-  { id: 2, title: "Post 2", content: "This is post 2" },
-  { id: 3, title: "Post 3", content: "This is post 3" },
+  { id: "1", title: "Post 1", content: "This is post 1" },
+  { id: "2", title: "Post 2", content: "This is post 2" },
+  { id: "3", title: "Post 3", content: "This is post 3" },
 ];
 app.use("*", prettyJSON());
 
@@ -14,7 +14,7 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.get("/posts", (c) => c.json({ posts: testPosts }));
+app.get("/get", (c) => c.json({ posts: testPosts }));
 
 app.get("/posts/:id", (c) => {
   const id = c.req.param("id");
@@ -25,6 +25,17 @@ app.get("/posts/:id", (c) => {
   } else {
     return c.json({ message: "not found" }, 404);
   }
+});
+
+app.post("/posts", async (c) => {
+  const { title, content } = await c.req.json<{
+    title: string;
+    content: string;
+  }>();
+  const newPost = { id: String(testPosts.length + 1), title, content };
+
+  testPosts = [...testPosts, newPost];
+  return c.json(newPost, 201);
 });
 
 export default app;
